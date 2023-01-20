88244b190abe323510e92fa447c97723234b97a4
ChristianL01 <locke.christian1@gmail.com>
2023-01-20 16:06:29 -0600

    Setup base game of RPS

--- Program.cs
+++ Program.cs
@@ -1,5 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//Random 
 Random random = new Random();
 int rand_num = random.Next(0, 3);
 System.Console.WriteLine(rand_num);
+string userInput = Console.ReadLine();
+System.Console.WriteLine(userInput);
+if (rand_num == 0) {
+    System.Console.WriteLine("Computer Chooses Rock!");
+
+    switch (userInput)
+    {
+        case "r":
+        System.Console.WriteLine("It's a Tie!");
+        break;
+        case "p":
+        System.Console.WriteLine("Player Wins!!");
+        break;
+        default:
+        System.Console.WriteLine("Computer Wins!");
+        break;
+    }
+}
+else if (rand_num == 1) {
+    System.Console.WriteLine("Computer Chooses Paper!");
+
+        switch (userInput)
+    {
+        case "r":
+        System.Console.WriteLine("Computer Wins!");
+        break;
+        case "p":
+        System.Console.WriteLine("It's a Tie!");
+        break;
+        default:
+        System.Console.WriteLine("Player Wins!");
+        break;
+    }
+}
+else {
+    System.Console.WriteLine("Computer Chooses Scissors!");
+
+        switch (userInput)
+    {
+        case "r":
+        System.Console.WriteLine("Player Wins!");
+        break;
+        case "p":
+        System.Console.WriteLine("Computer Wins!");
+        break;
+        default:
+        System.Console.WriteLine("It's a Tie!");
+        break;
+    }
+}
