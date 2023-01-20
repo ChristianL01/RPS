1f58d771af235aa6a0678352b45de578650424d3
ChristianL01 <locke.christian1@gmail.com>
2023-01-20 15:19:03 -0600

    Create C# app for Rock paper scissors

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Random random = new Random();
+int rand_num = random.Next(0, 3);
+System.Console.WriteLine(rand_num);
